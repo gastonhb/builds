0da80629712eeef1eaf4526cfc3c304dc4aad17b
Gaston <gaston.herrerabaron@outlook.com>
2023-02-02 01:03:36 -0300

    vercel configuration 7

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
 // Forward Vercel requests to normal index.php
- require __DIR__ . '\..\resources\views\checkArtifact\index.blade.php';
+ //require __DIR__ . '\..\resources\views\checkArtifact\index.blade.php';
+ require __DIR__.'/../public/index.php';
 //phpinfo();
-?>
